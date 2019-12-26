841ada7d19183b516926730d2e349842442ca3dc
Maud Gautier <maud.j.gautier@gmail.com>
2019-12-26 12:00:15 +0100

    Added mapping step
    Added the configuration files (standard and cluster), the documentation
    and the core script.

---
+++ docs/core/02_mapping.md
@@ -0,0 +1,46 @@
+README for ``02_mapping.bash``
+==============================
+
+Description
+-----------
+
+This script maps paired-end reads onto a reference genome, sorts and indexes the alignment, marks duplicates, filter reads to keep only the proper fragments and remove those that are unmapped or mapped as secondary (see SAMTools flag specifications) and subset the reads to those mapped on the regions reported in the BED file given as parameter.
+
+
+Usage
+-----
+
+```
+Usage: bash ./02_mapping.bash [-s SUBMISSION_FILE] [-t THREADS] -c CONFIG -f1 FASTQ_READ1 -f2 FASTQ_READ2 -o PREFIX_OUTPUT_BAM -g GENOME -b BEDFILE 
+  Options:
+   -s SUBMISSION_FILE
+             File containing the settings to submit the job on a cluster
+   -t THREADS
+             Number of threads
+  Parameters:
+   -c, --config, --source CONFIG
+             File containing the settings of the sample processed
+   -f1, --fastq1 FASTQ_READ1
+             One of the two FASTQ files (paired-end reads)
+   -f2, --fastq2 FASTQ_READ2
+             One of the two FASTQ files (paired-end reads)
+   -o, --output_prefix PREFIX_OUTPUT_BAM
+             Prefix of the output BAM file (without the file extension)
+   -g, --genome GENOME_FASTA
+             FASTA file of the genome to map onto. The FASTA file must have 
+             been indexed beforehand with bwa index
+   -b, --bedfile BEDFILE
+             BED file containing a list of intervals
+```
+
+
+Requirements
+------------
+
+* bwa 0.7.17
+* samtools 1.9
+* PicardTools 1.98
+* Bedtools v2.26.0
+
+
+
---
+++ src/core/02_mapping.bash
@@ -0,0 +1,158 @@
+#!/usr/bin/env bash
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                           GLOBAL VARIABLES                            ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Default parameters
+num_threads=1
+
+# Get parameters
+while [[ $# -gt 1 ]]
+do
+	key="$1"
+
+	case $key in
+		-c|--config|--source)
+			config_file="$2"
+			shift
+			;;
+		-o|--output_prefix)
+			output_prefix="$2"
+			shift # past argument
+			;;
+		-f1|--fastq1)
+			fastq1="$2"
+			shift # past argument
+			;;
+		-f2|--fastq2)
+			fastq2="$2"
+			shift # past argument
+			;;
+		-g|--genome)
+			genome_fasta="$2"
+			shift # past argument
+			;;
+		-t|--threads)
+			num_threads="$2"
+			shift
+			;;
+		-b|--bedfile)
+			intervals_bedfile="$2"
+			shift # past argument
+			;;
+		-s|--sub)
+			sub_file="$2"
+			shift
+			;;
+		*)
+			# unknown option
+			;;
+	esac
+	shift # past argument or value
+done
+
+echo CONFIG FILE	 = "${config_file}"
+echo OUTPUT PREFIX   = "${output_prefix}"
+echo INPUT FASTQ 1   = "${fastq1}"
+echo INPUT FASTQ 2   = "${fastq2}"
+echo GENOME FASTA	 = "${genome_fasta}"
+echo INTERVALS BED	 = "${intervals_bedfile}"
+echo NUM THREADS     = "${num_threads}"
+echo SUBMISSION FILE = "${sub_file}"
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                                SOURCE                                 ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+source $config_file
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                  MAPPING WITH BWA MEM ON DOMESTICUS                   ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Map reads on the genome
+bwa mem -M $genome_fasta \
+	-t $num_threads \
+	$fastq1 \
+	$fastq2 \
+	> ${output_prefix}.sam
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                             PREPROCESSING                             ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Transform sam -> bam
+samtools view -bhS ${output_prefix}.sam -o ${output_prefix}.bam
+
+# Sort alignments 
+samtools sort -o ${output_prefix}.sorted.bam ${output_prefix}.bam
+
+# Index
+samtools index ${output_prefix}.sorted.bam
+
+# Remove temporary files
+rm -f ${output_prefix}.sam
+rm -f ${output_prefix}.bam
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                            MARK DUPLICATES                            ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Mark Duplicates
+java -jar $PICARD/MarkDuplicates.jar \
+	METRICS_FILE=${output_prefix}.markDup_metrics.txt \
+	INPUT=${output_prefix}.sorted.bam \
+	OUTPUT=${output_prefix}.sorted.markedDup.bam
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####               FILTER DATA TO GET PROPER FRAGMENTS ONLY                ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Filter data
+samtools view -h \
+	-f 0x2 \
+	-F 0x4 \
+	-F 0x8 \
+	-F 0x100 \
+	${output_prefix}.sorted.markedDup.bam \
+	> ${output_prefix}.sorted.markedDup.only_mapped_fragments.only_primary.sam
+
+# Turn to Bam file
+samtools view \
+	-bhS ${output_prefix}.sorted.markedDup.only_mapped_fragments.only_primary.sam \
+	-o ${output_prefix}.sorted.markedDup.only_mapped_fragments.only_primary.bam   
+
+# Remove temporary file
+rm -f ${output_prefix}.sorted.markedDup.only_mapped_fragments.only_primary.sam
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                 SEPARATING INTO INTERVALS AND TARGETS                 ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Intervals (for the GATK pipeline)
+intersectBed -abam ${output_prefix}.sorted.markedDup.only_mapped_fragments.only_primary.bam \
+	-b $intervals_bedfile \
+	> ${output_prefix}.sorted.markedDup.only_mapped_fragments.only_primary.onIntervals.bam
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                            REMOVE PBS FILE                            ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+if [ ! -z $sub_file  ] ; then
+	if [ -s ${output_prefix}.sorted.markedDup.only_mapped_fragments.only_primary.bam ] ; then
+		rm -f $sub_file
+	fi
+fi
+
+
+
