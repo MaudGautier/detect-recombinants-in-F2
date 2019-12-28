5b748e619515c351f72527cbb064a7b63af5b73f
Maud Gautier <maud.j.gautier@gmail.com>
2019-12-28 09:21:02 +0100

    Added the genotyping reads step
    Added the documentation and the core code for this step.

---
+++ docs/core/04a_genotype_reads.md
@@ -0,0 +1,74 @@
+README for ``04a_genotype_reads.bash``
+======================================
+
+Description
+-----------
+
+This script genotypes all reads by creating a TSV file, reannotating alignments on the TSV (so that they are recorded in a format constistent with those of VCF files), reducing the TSV to the sole variants reported in the VCF file and genotyping each variant of each read sequenced by comparing the reference (REF) and alternate (ALT) alleles of a given variant (given by the VCF file) with the nucleotide sequenced (BASE) and the nuclotide on which it is aligned (REF) of each read (given by the TSV file).
+Additionally, because TSV files are extremely large (see [the memory requirements section of the README](https://github.com/MaudGautier/detect-recombinants-in-F1#memory-requirements)), this script begins with the reduction of the input BAM file to a smaller subset (defined by an input BED file), and the name of the targeted subset is added in the output file containing the genotyped reads.
+
+The output file is in the following format:
+```
+#CHROM  POS		  REF_VCF  ALT_VCF  READ_ID                                  FLAG  BASE_TSV  REF_TSV  MUT_TYPE  QUAL  FILTER_VCF  NB_VCF  FREQ_VCF  TARGET                                        GENOTYPE
+chr1    3882053	  A        G        D00350:383:H3FCCBCX2:1:2208:20909:16630  163   G         A        SNP       28    PASS        35      0.371429  Control.chr1_3881920_3882919/3880920/3883919  CAST
+chr1    6690792	  A        G        D00350:383:H3FCCBCX2:1:1105:16837:18805  99    A         A        SNP       27    PASS        128     0.398438  Control.chr1_6690920_6691919/6689920/6692919  B6
+chr1    6690788	  CA       C        D00350:383:H3FCCBCX2:1:1105:16837:18805  99    C         C        DEL       26    PASS        120     0.416667  Control.chr1_6690920_6691919/6689920/6692919  B6
+chr1    6690791	  C        CT       D00350:383:H3FCCBCX2:1:1105:16837:18805  99    C         C        INS       27    PASS        128     0.398438  Control.chr1_6690920_6691919/6689920/6692919  B6
+chr1	6691168	  GTATGC   G		D00350:383:H3FCCBCX2:1:1101:13116:17366  163   G		 GTATGC	  DEL		29	  PASS		  475	  0.524211  Control.chr1_6690920_6691919/6689920/6692919  CAST
+chr1	6691501	  G		   GTCCC	D00350:383:H3FCCBCX2:1:1101:13871:34877  99    GTCCC	 G		  INS		21	  PASS		  661	  0.481089  Control.chr1_6690920_6691919/6689920/6692919  CAST
+```
+
+
+Helpful notes
+-------------
+
+On TSV files, alignments are reported vertically, i.e. one line corresponds to one nucleotide sequenced. For further information about this file format, refer to [the ``sam2tsv`` tool documentation page](http://lindenb.github.io/jvarkit/Sam2Tsv.html).
+
+The output file contains information on which filters will be set in the ``04b_extract_recombinants.bash`` script. In particular, the filters on the quality score of variants are reported in the ``QUAL`` column. As such, performing this process on a BAM file for which base quality scores have been recalibrated with the BQRS step of GATK will allow to filter on more accurate base quality scores. I thus recommended to use the ``recal_reads.bam`` as the input BAM file for this script.
+
+
+Usage
+-----
+
+```
+Usage: bash ./04a_genotype_reads.bash [-s SUBMISSION_FILE] -c CONFIG -o OUTPUT_PREFIX -t TMP_PREFIX -i INPUT_BAM -f FREQ_VCF_FILE -b BED_FILE -g GENOME_FASTA -r REF_GENOME_NAME -a ALT_GENOME_NAME 
+  Options:
+   -s, --sub SUBMISSION_FILE
+             File containing the settings to submit the job on a cluster
+  Parameters:
+   -c, --config, --source CONFIG
+             File containing the settings of the sample processed
+   -o, --output_prefix OUTPUT_PREFIX
+             Prefix of the output file (without the file extension)
+   -t, --tmp_prefix TMP_PREFIX
+             Prefix of the temporary files (without the extension)
+   -i, --input_bam INPUT_BAM
+             Input BAM file (mapped on the same reference genome as that given
+             in parameter -g)
+   -f, --freq_vcf_file FREQ_VCF_FILE
+             File containing the frequencies of variant alleles (obtained 
+             by running the src/utils/prepare_freq_vcf_file.bash script on
+             a VCF file)
+   -g, --genome GENOME_FASTA
+             FASTA file of the genome to map onto. The FASTA file must have 
+             been indexed beforehand with bwa index
+   -b, --bed_file BEDFILE
+             BED file containing a list of intervals
+   -r, --ref_name REF_GENOME_NAME
+             Name of the genome used as a reference
+   -a, --alt_name ALT_GENOME_NAME
+             Name of the genome used as a alternative
+```
+
+
+Requirements
+------------
+
+* java 1.8
+* sam2tsv
+* Bedtools v2.26.0
+* Samtools 1.9
+* GNU Awk 4.2.1
+* GNU bash 4.4
+* GNU coreutils 8.30
+
---
+++ src/core/04a_genotype_reads.bash
@@ -0,0 +1,194 @@
+#!/usr/env/bin bash
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                              PARAMETERS                               ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Get parameters
+while [[ $# -gt 1 ]]
+do
+	key="$1"
+	
+	case $key in 
+		-c|--config|--source)
+			config_file="$2"
+			shift
+			;;
+		-t|--tmp_prefix)
+			tmp_prefix="$2"
+			shift # past argument
+			;;
+		-i|--input_bam)
+			input_bam="$2"
+			shift
+			;;
+		-f|--freq_vcf_file)
+			freq_vcf_file="$2"
+			shift
+			;;
+		-o|--output_prefix)
+			output_prefix="$2"
+			shift
+			;;
+		-b|--bed_file)
+			bed_subset="$2"
+			shift
+			;;
+		-g|--genome)
+			genome="$2"
+			shift
+			;;
+		-r|--ref_name)
+			ref_name="$2"
+			shift
+			;;
+		-a|--alt_name)
+			alt_name="$2"
+			shift
+			;;
+		-s|--sub)
+			sub_file="$2"
+			shift
+			;;
+		*)
+			# unknown option
+			;;
+		esac
+		shift
+done
+
+# Print parameters
+echo CONFIG FILE	 = "${config_file}"
+echo OUTPUT PREFIX	 = "${output_prefix}"
+echo TMP PREFIX      = "${tmp_prefix}"
+echo INPUT FILE      = "${input_bam}"
+echo FREQ VCF FILE	 = "${freq_vcf_file}"
+echo BED SUBSET	     = "${bed_subset}"
+echo GENOME			 = "${genome}"
+echo REF GENOME NAME = "${ref_name}"
+echo ALT GENOME NAME = "${alt_name}"
+echo SUBMISSION FILE = "${sub_file}"
+
+# Create tmp and output directories
+if [ ! -d "${tmp_prefix%/*}" ] ; then mkdir -p "${tmp_prefix%/*}" ; fi
+if [ ! -d "${output_prefix%/*}" ] ; then mkdir -p "${output_prefix%/*}" ; fi
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                                SOURCE                                 ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+source $config_file
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                REDUCE BAM FILE TO THE SELECTED HOTSPOTS               ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+intersectBed -a $input_bam -b $bed_subset > ${tmp_prefix}.bam
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                            CREATE TSV FILES                           ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# 1. Create SAM files
+samtools view -h ${tmp_prefix}.bam > ${tmp_prefix}.sam
+
+# 2. Convert SAM to TSV
+sam2tsv -r $genome -o ${tmp_prefix}.tsv ${tmp_prefix}.sam
+
+# 3. Delete temporary SAM file
+rm -f ${tmp_prefix}.sam
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                       REANNOTATE THE ALIGNMENTS                       ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# 1. Get INDEL sequences and reannotate SNP/INS/DEL
+awk -f $PROJ_SRC_CORE/utils/reannotate_INDELs_in_tsv.awk \
+	${tmp_prefix}.tsv \
+	> ${tmp_prefix}.annotated.tsv
+rm -f ${tmp_prefix}.tsv
+
+# 2. Sort the file
+bash $PROJ_SRC_CORE/utils/parallel_sort.bash \
+	${tmp_prefix}.annotated.tsv \
+	${tmp_prefix}.annotated.sorted.tsv \
+	3 \
+	50000000
+rm -f ${tmp_prefix}.annotated.tsv
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                         REDUCE TSV TO VARIANTS                        ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# 1. Join FREQ_VCF and TSV records
+join -1 1 -2 3 \
+	-t $'\t' \
+	-o 2.4,2.5,1.2,1.3,2.1,2.2,2.6,2.7,2.9,1.4,1.5,1.6,1.7 \
+	${freq_vcf_file} \
+	${tmp_prefix}.annotated.sorted.tsv \
+	> ${tmp_prefix}.annotated.onlyVariantPositions.MISSING.txt
+
+# 2. Sort joined records on ID
+awk -v OFS="\t" '{ 
+	print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11+$12, $13 
+	}' ${tmp_prefix}.annotated.onlyVariantPositions.MISSING.txt | \
+	sort -k5,5 | uniq \
+	> ${tmp_prefix}.annotated.onlyVariantPositions.sorted.txt
+
+# 3. Delete temporary file
+rm -f ${tmp_prefix}.annotated.onlyVariantPositions.MISSING.txt
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                           ADD INTERVAL NAME                           ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# 1. Associate each read to the locus it targets
+intersectBed -abam ${tmp_prefix}.bam -b $bed_subset -bed -wb | \
+	awk -v OFS="\t" '{split($4,a,"/") ; print a[1],$16"/"$14"/"$15}' | \
+	sort -k1,1 | uniq \
+	> ${tmp_prefix}.targets.txt
+
+# 2. Link the reduced file (TSV joined with VCF) with targets
+join -t $'\t' -1 5 -2 1 -a1 -e "NA" \
+	-o 1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10,1.11,1.12,2.2 \
+	${tmp_prefix}.annotated.onlyVariantPositions.sorted.txt \
+	${tmp_prefix}.targets.txt | sort -k1,1 -k2,2g | \
+	awk -v OFS="\t" '
+	BEGIN {
+		print "#CHROM", "POS", "REF_VCF", "ALT_VCF", "READ_ID", "FLAG", "BASE_TSV", "REF_TSV", "QUAL", "FILTER_VCF", "NB_VCF", "FREQ_VCF", "TARGET"
+	}
+	{print}' \
+	> ${tmp_prefix}.onlyVariantPositions.allFilters.sorted.targets.txt
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                         GENOTYPE EACH VARIANT                         ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+awk -v REF_GENOME=$ref_name -v ALT_GENOME=$alt_name \
+	-f $PROJ_SRC_CORE/utils/genotype_variants.awk \
+	${tmp_prefix}.onlyVariantPositions.allFilters.sorted.targets.txt \
+	> ${output_prefix}.Genotyped_ReadsxVariants.txt
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                        DELETE SUBMISSION FILE                         ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Delete temporary files
+rm -rf "${tmp_prefix%/*}"
+
+# Delete submission file if everything finished
+if [ ! -z $sub_file ] ; then
+	if [ -s ${output_prefix}.hq_recalibrated_variants.vcf ] ; then
+		rm -f $sub_file
+	fi
+fi
+
+
