aaa56272d38f03ad4eba2f92163cfdeeb7ba14c6
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-11 14:52:13 +0200

    Correct syntax

--- src/core/05a_genotype_reads.bash
+++ src/core/05a_genotype_reads.bash
@@ -190,7 +190,7 @@ rm -f ${tmp_prefix}.targets.txt
 
 # Delete submission file if everything finished
 if [ ! -z $sub_file ] ; then
-	if [ -s ${output_prefix}.hq_recalibrated_variants.vcf ] ; then
+	if [ -s ${output_prefix}.Genotyped_ReadsxVariants.txt ] ; then
 		rm -f $sub_file
 	fi
 fi
